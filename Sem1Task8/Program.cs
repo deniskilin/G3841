d2b6520336178999dec2021a68404779ea1eb1b2
DEN <cyberultrapro@gmail.com>
2023-01-22 21:49:06 +0300

    add homework

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Считываем данные с консоли
+string? imputNumLine = Console.ReadLine();
+int N = int.Parse(imputNumLine);
+string? result = string.Empty;
+for(int i=2; i<N; i=i+2)
+{
+    result = result + i + ',';
+}
+if(N%2==0)
+{
+    result = result + 2;
+}
+
+Console.WriteLine(result);
