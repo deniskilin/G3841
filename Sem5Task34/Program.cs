b55934f360c11d2f8d25a668d0e7f796754fb661
DEN <cyberultrapro@gmail.com>
2023-02-17 19:05:27 +0300

    add task 34

---
+++ Sem5Task34/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// *** Отсортировать массив методом пузырька
+
+// Метод вывода данных в консоль
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+// Метод генерации 1D массива
+int[] Gen1DArray(int len, int minValue, int maxValue)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return arr;
+}
+
+// Метод печати 1D массива в консоль
+void Print1DArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+// Метод подсчета элементов 1D массива по заданному условию
+int CountElem(int[] arr)
+{
+    int res = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (Test(arr[i]))
+        {
+            res++;
+        }
+    }
+    return res;
+}
+
+// Метод тест элемента массива на четность
+bool Test(int n)
+{
+    return (n % 2 == 0);
+}
+
+// Основной код программы:
+
+// Генерируем массив по заданным в задачах параметрам
+int[] testArr = Gen1DArray(10,100,999);
+
+// Выводим массив в консоль
+Print1DArr(testArr);
+
+// Подсчитываем кол-во четных элементов массива
+int count = CountElem(testArr);
+
+//Выводим ответ в консоль
+PrintData("Количество четных элементов равно: "+count);
